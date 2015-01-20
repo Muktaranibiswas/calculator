aa3d88f6a0cb5f9431c3379470b78f983fef77e4
Muktaranibiswas <muktaquate@yahoo.com>
2015-01-20 14:00:57 +0600

    making classtest

---
+++ website/classheader.php
@@ -0,0 +1,52 @@
+
+
+<header class="container">
+
+
+    <div class="row">
+        <div class="col-xs-12">
+            <br>
+            <img src="images/header.jpeg" width="1100" hight="200">
+        </div>
+    </div>
+
+
+</header>
+
+<div class="container">
+
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="classhome.html">Home</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">link <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Link1</a></li>
+                            <li><a href="#">Link2</a></li>
+                            <li><a href="#">link3</a></li>
+
+                            <li><a href="#">Link4</a></li>
+                        </ul>
+                    <li><a href="classabout.html">About</a></li>
+                    <li><a href="classcontact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+    </nav>
